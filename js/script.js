0cec31293ca9201cc3e9d7f831050d491a5ebbad
UserAbdullo <metalturbofergana@gmail.com>
2025-01-13 00:15:53 +0500

    end site loader and timer

---
+++ js/script.js
@@ -0,0 +1,110 @@
+window.addEventListener('DOMContentLoaded',() => {
+	//Tabs
+	const tabs = document.querySelectorAll('.tabheader__item'),
+		tabContents = document.querySelectorAll('.tab_content'),
+		tabParents = document.querySelector('.tabheader__items')
+
+		function hideTabContentds() {
+			tabContents.forEach(tabContent => {
+				tabContent.classList.add('hide')
+			})
+
+			tabs.forEach(tab => {
+				tab.classList.remove('tabheader__item_active')
+			})
+		}
+
+		function showTabContent(index = 0) {
+			tabContents[index].classList.add('show', 'fade')
+			tabContents[index].classList.remove('hide')
+			tabs[index].classList.add('tabheader__item_active')
+		}
+
+		hideTabContentds()
+		showTabContent()
+
+		tabParents.addEventListener('click', (event) => {
+			const target = event.target
+
+			if (target && target.classList.contains('tabheader__item')) {
+				tabs.forEach((tab , index) => {
+					if (target === tab) {
+						hideTabContentds()
+						showTabContent(index)
+					}
+				})
+			}
+		})
+
+	//Loaders
+	const loaderWrapper = document.querySelector('.loader-wrapper')
+
+	setTimeout(() =>{
+		loaderWrapper.style.display = 'none'
+	}, 1500)
+
+	//Timer
+
+	const deadLine = '2025-10-10'
+
+	function getTimeRemaining(endTime) {
+		const time = Date.parse(endTime) - Date.parse(new Date)
+
+		let days, hours, minutes, seconds
+
+		if (time <= 0) {
+			days = 0,
+			hours = 0,
+			minutes = 0,
+			seconds = 0
+		}else {
+			days = Math.floor(time / (1000*60*60*24)),
+			hours = Math.floor(time / (1000*60*60) %24),
+			minutes = Math.floor(time / (1000*60) %60),
+			seconds = Math.floor(time / (1000) %60)
+		}
+			
+			return {
+				totalTime: time,
+				days,
+				hours,
+				minutes,
+				seconds,
+			}
+		}
+
+	function formatNumber(number) {
+		if (number >=0 && number < 10){
+			return `0${number}`
+		}else {
+			return number
+		}
+	}
+			
+	function setClock(selector, endTime) {
+		const timer = document.querySelector(selector)
+
+			days = timer.querySelector('#days')
+			hours = timer.querySelector('#hours')
+			minutes = timer.querySelector('#minutes')
+			seconds = timer.querySelector('#seconds')
+			timeInterval = setInterval(updateClock, 1000)
+			
+		updateClock()
+
+			function updateClock() {
+				const time = getTimeRemaining(endTime)
+				
+				days.textContent = formatNumber(time.days)
+				hours.textContent = formatNumber(time.hours)
+				minutes.textContent = formatNumber(time.minutes)
+				seconds.textContent = formatNumber(time.seconds)
+				
+				if (time.totalTime <= 0 ) {
+					clearInterval(timeInterval)
+				}
+			}
+		}
+
+	setClock('.timer', deadLine)
+})
