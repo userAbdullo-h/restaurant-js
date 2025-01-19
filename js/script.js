6a119d2271491b1fbd58e4a9a71f34f4f90583ca
UserAbdullo <metalturbofergana@gmail.com>
2025-01-19 11:21:49 +0500

    update loader, Forms section for js, add telegram bot for site

--- js/script.js
+++ js/script.js
@@ -113,7 +113,7 @@ window.addEventListener('DOMContentLoaded',() => {
 
 	const modalOpenBtns = document.querySelectorAll("[data-modal]"),
 		modal = document.querySelector(".modal"),
-		modalCloseBtn = document.querySelector('[data-modal-close]'),
+		// modalCloseBtn = document.querySelector('[data-modal-close]'),
 		modalContent = document.querySelector('.modal__content')
 	
 		function openModal() {
@@ -135,10 +135,10 @@ window.addEventListener('DOMContentLoaded',() => {
 		btn.addEventListener("click", openModal)
 	})
 
-	modalCloseBtn.addEventListener('click', closeModal)
+	// modalCloseBtn.addEventListener('click', closeModal)
 
 	modal.addEventListener('click', (event) => {
-		if (event.target === modal) {
+		if (event.target === modal || event.target.getAttribute('data-modal-close') === '' ){
 			closeModal()
 		}
 	})
@@ -295,4 +295,77 @@ offers.forEach(offer => {
 		const {src, alt,title, descr} = dayTime
 		new DayTime(src,alt,title,descr,'.daytime-items').renderPhoto()
 	})
+
+	//FORMS
+
+	const form = document.querySelector("form"),
+		telegramTokenBot = '7631856448:AAH4XbQe4-0ehm0YeJw-_Bkjsf-WREf7yVY',
+		chatId = '6229059361'
+
+	const message = {
+		loading: 'Loading ...',
+		success: 'Thanking for contacting with us',
+		failure: 'Something went wrong'
+	}
+
+	form.addEventListener('submit', (event) =>{
+		event.preventDefault()
+
+		const loader = document.createElement('div')
+		loader.classList.add("loader")
+		loader.style.width = '20px'
+		loader.style.height = '20px'
+		loader.style.marginTop = '20px'
+		// loader.class
+		form.append(loader)
+
+		// form.append(statusMessage)
+		const formData = new FormData(form)
+
+		const object = {}
+		formData.forEach((value, key) =>{
+			object[key] = value
+		})
+
+		fetch(`https://api.telegram.org/bot${telegramTokenBot}/sendMessage`, {
+			 method: 'POST',
+			 headers: {"Content-Type": 'application/json'},
+			 body: JSON.stringify({
+				chat_id: chatId,
+				text: `
+					Name: ${object.name}, 
+					Phone: ${object.phone}`, 
+			 })
+		}).then(() => {
+			showStatusMessage(message.success)
+			form.reset()
+		}).catch(() => {
+			showStatusMessage(message.failure)
+		}).finally (() => loader.remove())
+	})
+
+	function showStatusMessage (message) {
+		const modalDialog = document.querySelector('.modal__dialog')
+
+		modalDialog.classList.add('hide')
+		openModal()
+
+		const statusModal = document.createElement('div')
+		statusModal.classList.add('modal__dialog')
+		statusModal.innerHTML = `
+			<div class="modal__content">
+				<div data-modal-close class="modal__close">&times;</div>
+				<div data-modal class="modal__title">${message}</div>
+			</div>
+		`
+
+		document.querySelector('.modal').append(statusModal)
+
+		setTimeout(() => {
+			statusModal.remove()
+			// modalDialog.classList.add('show')
+			modalDialog.classList.remove('hide')
+			closeModal()
+		}, 3000)
+	}
 })
